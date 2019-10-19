e830abca703c6f7006cd3c333534a8d871e536c0
Robert Frank <robert.allan.frank@gmail.com>
2019-10-18 22:27:59 -0500

    Add string comparison helpers (#6)
    * Add math comparison helpers
    
    * Update readme
    
    * Update to shift

---
+++ tests/integration/helpers/string-not-equals-test.js
@@ -0,0 +1,28 @@
+import { render } from '@ember/test-helpers';
+import { setupRenderingTest } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+import { module, test } from 'qunit';
+
+module('Integration | Helper | string-not-equals', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('can compare two values', async function(assert) {
+    await render(hbs`{{string-not-equals 'ember' 'ember'}}`);
+    assert.equal(this.element.textContent.trim(), 'false');
+  });
+
+  test('can compare two not equal values', async function(assert) {
+    await render(hbs`{{string-not-equals 'ember' 'react'}}`);
+    assert.equal(this.element.textContent.trim(), 'true');
+  });
+
+  test('can compare n values', async function(assert) {
+    await render(hbs`{{string-not-equals 'hello' 'hello' 'hello' 'hello' 'hello'}}`);
+    assert.equal(this.element.textContent.trim(), 'false');
+  });
+
+  test('can compare n not equal values', async function(assert) {
+    await render(hbs`{{string-not-equals 'hello' 'hello' 'hello' 'hello' 'goodbye'}}`);
+    assert.equal(this.element.textContent.trim(), 'true');
+  });
+});
